59729988d60d738053af9b93b5fff9a6997bc1c5
Nguyen Van Tien <nguyenvantien080174@gmail.com>
2025-03-02 12:26:22 +0700

    basic UI

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -1,3 +1,11 @@
 document.querySelector("form").addEventListener("submit", function(e) {
     alert("Đang tìm kiếm chuyến bay...");
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.querySelector(".booking-form form");
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+        alert("Chức năng đang được cập nhật!");
+    });
+});
