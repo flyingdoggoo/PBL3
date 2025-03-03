c745a88ce7874e0354904eb3d4509a7ec217ef23
k <162461123+4UT15T1Ck@users.noreply.github.com>
2025-03-03 14:19:17 +0700

    feat:basic login page

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -1,3 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+
 document.querySelector("form").addEventListener("submit", function(e) {
     alert("Đang tìm kiếm chuyến bay...");
 });
